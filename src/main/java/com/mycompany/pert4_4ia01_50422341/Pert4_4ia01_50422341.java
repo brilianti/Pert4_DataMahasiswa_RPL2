c06b2e76b33ae0624eca6d492f06a5870d9f449d
Brilianti <briliantii68@gmail.com>
2025-11-25 16:39:29 +0700

    first commit

---
+++ src/main/java/com/mycompany/pert4_4ia01_50422341/Pert4_4ia01_50422341.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pert4_4ia01_50422341;
+
+/**
+ *
+ * @author Brilianti
+ */
+public class Pert4_4ia01_50422341 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
